c35bba9d2b0b53c2f22676f03ceb3e9ec33e80ac
vsreddyh <shouryanreddyh@gmail.com>
2025-03-03 09:43:44 +0530

    fixed nvim battery issue and added more configs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,6 @@ config = wezterm.config_builder()
 
 config = {
 	automatically_reload_config = true,
-	enable_tab_bar = false,
 	window_close_confirmation = "NeverPrompt",
 	window_decorations = "RESIZE",
 	default_cursor_style = "BlinkingBar",
